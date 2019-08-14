2288310503a95c1f0085f8c94472e067f0ae011a
BraianM2000 <braian.morales@correounivalle.edu.co>
2019-08-14 12:54:06 -0500

    Creando rutas

--- server/server.js
+++ server/server.js
@@ -13,9 +13,32 @@ app.use(express.static(path.join(__dirname,'../dist/')));
 //Routes
 app.get('/*/', (req, res) => {
     res.sendFile(path.join(__dirname,'../dist/index.html'));
-})
+});
+
+//Signup
+
+app.get('/signup',(req,res)=>{
+    res.send('aquí va la vista del registro');
+});
+
+app.post('/signup/user',(req,res)=>{
+    const {name, email, password, confirmpassword} = req.body;
+    res.send('Te has registrado correctamente');
+});
+
+//Login.
+
+app.get('/signin',(req,res)=>{
+    res.send('aquí va la vista de inicio de sesión');
+});
+
+app.post('/signin/user',(req,res)=>{
+    const {email, password} = req.body;
+    res.send('Inicio de sesión correcto');
+});
+
 
 //Startint
 app.listen(app.get('port'), () => {
     console.log("Stating server " + app.get('port'));
-})
+});
